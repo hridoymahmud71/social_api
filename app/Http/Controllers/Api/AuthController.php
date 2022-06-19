7ac7ee8131926a8ca63bff4b39572df732392292
hridoymahmud71 <hridoymahmud71@gmail.com>
2022-06-19 19:32:15 +0600

    login done

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Requests\Api\LoginRequest;
 use App\Utility\UserUtility;
 use Auth;
 use App\Http\Resources\UserResource;
@@ -14,28 +15,6 @@ class AuthController extends Controller
     public function register(RegisterRequest $request)
     {
 
-
-        // $messages = [
-        //     'name.required' => 'Name is required',
-        //     'name.max' => 'Maximum 255 characters for name',
-        //     'email.required' => 'Email is required',
-        //     'email.email' => 'Email must be a valid email address',
-        //     'email.unique' => 'An user exists with this email',
-        //     'password.required' => 'Password is required',
-        //     'password.confirmed' => 'Password confirmation does not match',
-        //     'password.min' => 'Minimum 6 digits required for password',
-        // ];
-
-        // $validator = Validator::make($request->all(), [
-        //     'name' => 'required|string|max:255',
-        //     'email' => 'required|string|email|unique:users,email',
-        //     'password' => 'required|string|min:6|confirmed'
-        // ], $messages);
-
-
-        // if ($validator->fails()) {
-        //     return response()->json(['result' => false, 'message' => 'Request is not valid', 'errors' => $validator->errors(), 'user' => null], 404);
-        // }
 
         $user =  UserUtility::create_user($request->all());
 
@@ -70,9 +49,16 @@ class AuthController extends Controller
         );
     }
 
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
-        echo "login";
+        if (!Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return response()->json(['result' => false, 'message' => 'Login Failed', 'user' => null], 401);
+        }
+
+        return $this->login_success(
+            auth()->user()->createToken('API Token')->plainTextToken,
+            auth()->user()
+        );
     }
 
     public function logout()
